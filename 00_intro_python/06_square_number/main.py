833601d6bc195575017b08305ea88f4a5d0d626c
ISMAIL AHMED SHAH <ismailahmedshahpk@gmail.com>
2025-03-20 06:27:34 +0500

    06 assignments done

--- 00_intro_python/06_square_number/main.py
+++ 00_intro_python/06_square_number/main.py
@@ -1,14 +1,32 @@
+# def main():
+#     print("\nWelcome to the Square Number Program!")
+#     while True:
+#         try:
+#             num = float(input("\nType a number to see its square: "))
+#             print(str(num) + " squared is " + str(num ** 2))
+#             break  # Exit the loop if input is valid
+#         except ValueError:
+#             print("\n❌ Invalid input! Please enter a valid number.")
+
+
+# # There is no need to edit code beyond this point
+# if __name__ == '__main__':
+#     main()
+
 def main():
-    print("\nWelcome to the Square Number Program!")
-    while True:
-        try:
-            num = float(input("\nType a number to see its square: "))
-            print(str(num) + " squared is " + str(num ** 2))
-            break  # Exit the loop if input is valid
-        except ValueError:
-            print("\n❌ Invalid input! Please enter a valid number.")
+            print("\nWelcome to the Square Number Program!")
+
+            while True:
+                try:
+                    num = float(input("\nType a number to see its square: "))
+
+                    print(f"\n{num} squared is {num ** 2}\n")
+                    break
+
+                except ValueError:
+                    print("\n❌ Invalid input! Please enter a valid number.")
 
 
-# There is no need to edit code beyond this point
 if __name__ == '__main__':
     main()
+
